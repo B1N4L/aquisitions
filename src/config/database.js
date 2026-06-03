1be826cc187f6d80d6b6e673e457985ab23ff7b8
B1N4L <163591915+B1N4L@users.noreply.github.com>
2026-06-03 09:54:17 +0530

    feat: configure neon local environment for development and add production Docker script

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,14 @@
 import 'dotenv/config.js';
 
-import {neon} from '@neondatabase/serverless';
+import {neon, neonConfig} from '@neondatabase/serverless';
 import {drizzle} from 'drizzle-orm/neon-http';
 
+// referred from neon local documentation for setting up neon local environment
+if(process.env.NODE_ENV === 'development') {
+  neonConfig.fetchEndpoint = 'http://neon-local:5432/sql'; // refer endpoint and port for neon-local from docker.compose.dev.yml
+  neonConfig.useSecureWebSocket = false;
+  neonConfig.poolQueryViaFetch = true;
+}
 const sql = neon(process.env.DATABASE_URL);
 
 const db = drizzle(sql);
